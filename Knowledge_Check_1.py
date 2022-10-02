9f6b997eba5088bdf30510c02352c4f4f2ddebab
Adam Cohen <adamscohen94@live.com>
2022-10-02 16:41:32 -0400

    Knowledge Check 1 Complete

---
+++ Knowledge_Check_1.py
@@ -0,0 +1,20 @@
+# a statement that prints "hello world!"
+
+print("hello world!")
+
+# a list populated with several values. They can be strings / integers / floats etc. 
+# Then, print() one of those values either to the prompt or in a cell (if using Jupyter Notebook)
+
+kc1_list = ['1a', '2b', '3c']
+print(kc1_list[1])
+
+# a dictionary populated with two keys and two values. 
+# Print one of the values just like you did above.
+
+kc1_dict = {'key1':'value1', 'key2':'value2'}
+print(kc1_dict['key1'])
+
+# a tuple with 4 values. Print one of them.
+
+kc1_tuple = ('z1', 'y2', 'x3', 'w4')
+print(kc1_tuple[3])
